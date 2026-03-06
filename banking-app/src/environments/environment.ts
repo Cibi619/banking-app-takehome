74858f982705d8e9700b52762c5ffbe5623216d5
cibi619 <cibi.mit@gmail.com>
2026-03-06 10:17:36 -0500

    Initial setup

---
+++ banking-app/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase:  {
+        apiKey: "AIzaSyBhQ_R_cDGFx243F_Cd1T4PMkZ24bSCtlc",
+        authDomain: "angular-banking-app-e3078.firebaseapp.com",
+        projectId: "angular-banking-app-e3078",
+        storageBucket: "angular-banking-app-e3078.firebasestorage.app",
+        messagingSenderId: "463653495348",
+        appId: "1:463653495348:web:46c4fefde9297426bd2061",
+        measurementId: "G-R88H7E4VNQ"
+    }
+};
